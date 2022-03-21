24e6c82de82891b28721b5c0c893bf3c5e58695e
Chaitanya Dhiman <78900698+ChaitanyaDhiman@users.noreply.github.com>
2022-03-21 14:24:07 +0530

    Add files via upload

---
+++ projectaop/src/main/java/projectaop/TestAoP.java
@@ -0,0 +1,21 @@
+package projectaop;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestAoP {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		try {
+			@SuppressWarnings("resource")
+			ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+			Customer tc = (Customer) context.getBean("customer");
+			tc.addCustomer();
+		} catch (Exception e) {
+			// e.printStackTrace();
+		}
+
+	}
+
+}
